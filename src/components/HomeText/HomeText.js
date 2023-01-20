205abdf3c0382608e646aa6c28332f5f1b2f3f6a
svetoslavmanolov <svn.manolov@gmail.com>
2023-01-20 13:39:46 +0200

    feat: create Hometext component

---
+++ src/components/HomeText/HomeText.js
@@ -0,0 +1,17 @@
+import './HomeText.css'
+
+
+const HomeText = () => {
+    return (
+        <>
+            <div className="main-div-hometext">
+                <p className="text-1">The fastest brunch delivery in <span>Your city</span></p>
+            </div>
+            <div className="second-div-hometext">
+                <p className="text-2">This is my Exploration for Fruit - Food Delivery Landing Page 🍕. How about you ?</p>
+            </div>
+        </>
+    );
+}
+
+export default HomeText;
