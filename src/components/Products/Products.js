87a8a6d630f3b4541d2b4da28b0f211b2fe58b5a
svetoslavmanolov <svn.manolov@gmail.com>
2023-01-21 19:56:12 +0200

    fix: fix bug related to scrolling of products Removing collision between scrolling of products and adding each individual product to the basket

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -12,6 +12,23 @@ const Products = ({ setOrderModal, setBasketModal, basketModal }) => {
     const { state, dispatch } = useContext(BrunchyContext);
     const { cart } = state;
 
+    const [startPosition, setStartPosition] = useState(0);
+    const [endPosition, setEndPositon] = useState(0);
+
+    const scrollRef = useRef(null);
+    
+    const mouseDown = (e) => {
+        setStartPosition(e.clientX);
+    };
+    
+    const mouseUp = (e) => {
+        setEndPositon(e.clientX);
+    }
+
+    const scroll = () => {
+        scrollRef.current.scrollLeft += (startPosition - endPosition);
+    }
+
     const addToCartHandler = async (product) => {
         const existingItem = cart.cartItems.find((item) => item.name === product.name);
         const quantity = existingItem ? existingItem.quantity + 1 : 1;
@@ -21,10 +38,10 @@ const Products = ({ setOrderModal, setBasketModal, basketModal }) => {
 
     return (
         <>
-            <div className="main-product-div">
+            <div ref={scrollRef} onMouseDown={mouseDown} onMouseUp={mouseUp} onClick={scroll} className="main-product-div">
                 {products.items.length > 0
                     ? products.items.map(x => <ProductItem
-                        key={uuid()}
+                        key={x.name}
                         product={x}
                         addToCartHandler={addToCartHandler}
                     />)
